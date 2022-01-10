dd2c846ca646a3847258e5938d41bd6553ea63bf
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2022-01-10 21:15:58 +0500

    table crud

--- src/App.js
+++ src/App.js
@@ -1,121 +1,97 @@
-import React, { Component } from 'react';
-import { Data } from './data';
+import React, { useState } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: null,
-      name: '',
-      status: '',
-      age: '',
-      data: {
-        dataList: Data,
+export const App = () => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: 'Qobiljon',
+      status: 'Student',
+    },
+    {
+      id: 2,
+      name: 'Toshmat',
+      status: 'Mentor',
+    },
+    {
+      id: 3,
+      name: 'Michael',
+      status: 'Unemployed',
+    },
+    {
+      id: 4,
+      name: 'Mike',
+      status: 'Lawyer',
+    },
+  ]);
+
+  /* -------Function Delete-------- */
+
+  const getDelete = (ids) => {
+    const newData = data.filter((value) => value.id !== ids);
+    setData(newData);
+  };
+  /* -------Function Delete-------- */
+
+  /* -------Function Create-------- */
+  const [names, setNames] = useState('');
+
+  const getSave = () => {
+    const AddData = [
+      ...data,
+      {
+        id: data.length + 1,
+        name: names,
       },
-    };
-  }
+      
+    ];
+    setData(AddData);
+  };
+  /* -------Function Create-------- */
 
-  render() {
-    
-    const onDelete = (id) => {
-      let res = this.state.data.dataList.filter((item) => item.id !== id);
-      this.setState({
-        data: {
-          ...this.state.data,
-          dataList: res,
-        },
-      });
-    };
+  /* -------Function Edit-------- */
+  const [Select, setSelect] = useState(null);
 
-    const onChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value,
-      });
-    };
+  const getEdit = (value) => {
+    setSelect(value.id);
+  };
 
-    const onSave = () => {
-      const newData = {
-        id: Date.now(),
-        name: this.state.name,
-        status: this.state.status,
-        Age: this.state.age,
-      };
-      this.state.name.length > 0 ?
-        this.setState({
-          data: {
-            ...this.state.data,
-            dataList: [...this.state.data.dataList, newData],
-          },
-          name: '',
-          status: '',
-          age: '',
-        }) : alert('Please Fill the input')
-    };
-
-    const onEdit = (id) => {
-      this.setState({active:id})
-    }
-
-
-    return (
-      <div className='wrapper'>
-        <table border='1' width='100%'>
-          <thead >
-            <tr>
-              <th >ID</th>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Age</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.data.dataList.map(( { id, name, status, Age },) => (
-              <tr key={id}>
+  return (
+    <div>
+      <input
+        onChange={(e) => setNames(e.target.value)}
+        type='text'
+        name=''
+        placeholder='Your Name'
+      />
+      <button onClick={getSave}>save</button>
+      <table border='1' style={{ width: '50%' }}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value, index) => {
+            return (
+              <tr key={value.id}>
+                <td>{index + 1}</td>
+                <td>{value.name}</td>
                 <td>
-                  {
-                    this.state.active === id ? <input value={id}/> : id
-                  }
-                </td>
-                <td>{name}</td>
-                <td>{status}</td>
-                <td>{Age}</td>
-                <td>
-                  <button onClick={() => onDelete(id)}>delete</button>
-                  <button onClick={() => onEdit(id)}>
-                    {this.state.active === id ? 'save' : 'edit'}
-                  </button>
+                  <button onClick={() => getDelete(value.id)}>delete</button>
+                  {Select === value.id ? (
+                    <button>save</button>
+                  ) : (
+                    <button onClick={() => getEdit(value)}>edit</button>
+                  )}
                 </td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <input
-          onChange={onChange}
-          placeholder='name'
-          type='text'
-          name='name'
-          value={this.state.name}
-        />
-        <input
-          onChange={onChange}
-          placeholder='status'
-          type='text'
-          name='status'
-          value={this.state.status}
-        />
-        <input
-          onChange={onChange}
-          placeholder='Age'
-          type='text'
-          required
-          name='age'
-          value={this.state.age}
-        />
-        <button onClick={onSave}>Add</button>
-        <br />
-      </div>
-    );
-  }
-}
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 export default App;
