1ace0c905c0c871f34860def1796e23f673f7de5
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2022-01-22 14:42:42 +0500

    some changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Context } from "./Context/context";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Context>
+      <App/>
+    </Context>
+      
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+
